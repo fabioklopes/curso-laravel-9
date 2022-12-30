337c6f546b47e52c02a097e016c5900d98b010e2
Fábio Klevinskas Lopes <fabioklopes@live.com>
2022-12-30 13:14:38 -0300

    Criação de Controllers de exemplos

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\ContatoController;
+use App\Http\Controllers\QuemSomosController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 
 Route::get('/users', [UserController::class, 'index'])->name('users.index');
+Route::get('/quemsomos', [QuemSomosController::class, 'index'])->name('quemsomos.index');
+Route::get('contato', [ContatoController::class, 'index'])->name('contato');
+
 Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show');
 
 Route::get('/', function () {
